0c7ae0619d019a1409cec8fe6e9a7fc68f61d4d5
johndev <jahongirnagmatov@gmail.com>
2021-07-04 18:46:04 +0500

    add django authentication system and fix urls bugs

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('siteusers.urls')),
     path('admin/', admin.site.urls),
-    path('dashboard/', include('siteusers.urls')),
-
 ]
