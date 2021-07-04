1c43eefaeedcd76de1112d6ec9ecac2e9699cefd
johndev <jahongirnagmatov@gmail.com>
2021-07-04 19:00:23 +0500

    add login template

--- siteusers/urls.py
+++ siteusers/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("acoounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("dashboard/", views.dashboard, name="dashboard"),
 ] 
