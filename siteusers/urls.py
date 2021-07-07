728e25afb9eb0cf8a5177ffb6e5b66bfff227df2
Jahongir <jahongirnagmatov@gmail.com>
2021-07-07 11:09:36 +0500

    add reset-password view and template

--- siteusers/urls.py
+++ siteusers/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
 
     path('accounts/change-password/',auth_views.PasswordChangeView.as_view(template_name='registration/change-password.html', success_url='done'),  name='change-password'),
     path('accounts/change-password/done', auth_views.PasswordChangeDoneView.as_view(template_name='registration/change-password-done.html'), name="change-password-done"),
+    path('accounts/reset-password', auth_views.PasswordResetView.as_view(template_name='registration/reset-password.html', success_url="done"), name="reset-password"),
+    path('accounts/reset-password/done', auth_views.PasswordResetConfirmView.as_view(template_name='registration/reset-password-done.html'), name="reset-password-confirm"),
     path("dashboard/", views.dashboard, name="dashboard"),
 ]
