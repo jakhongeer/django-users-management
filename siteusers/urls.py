0b125ec4044f22956610082e5c929bc8d42b0879
johndev <jahongirnagmatov@gmail.com>
2021-07-07 16:35:55 +0500

    delete reset password form

--- siteusers/urls.py
+++ siteusers/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
+from siteusers.views import dashboard, register 
 
 urlpatterns = [
     path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
@@ -8,7 +10,6 @@ urlpatterns = [
 
     path('accounts/change-password/',auth_views.PasswordChangeView.as_view(template_name='registration/change-password.html', success_url='done'),  name='change-password'),
     path('accounts/change-password/done', auth_views.PasswordChangeDoneView.as_view(template_name='registration/change-password-done.html'), name="change-password-done"),
-    path('accounts/reset-password', auth_views.PasswordResetView.as_view(template_name='registration/reset-password.html', success_url="done"), name="reset-password"),
-    path('accounts/reset-password/done', auth_views.PasswordResetConfirmView.as_view(template_name='registration/reset-password-done.html'), name="reset-password-confirm"),
     path("dashboard/", views.dashboard, name="dashboard"),
+    path("register/", register, name="register"),
 ]
