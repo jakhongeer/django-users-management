6c9dfb935c816a77736d9614e9d7526d1ec45a07
Jahongir Nagmatov <jahongirnagmatov@gmail.com>
2021-07-14 18:40:54 +0500

    fix merged conflicts

--- siteusers/urls.py
+++ siteusers/urls.py
@@ -6,11 +6,7 @@ from siteusers.views import dashboard, register
 
 
 urlpatterns = [
-    path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
-
-    path('accounts/change-password/',auth_views.PasswordChangeView.as_view(template_name='registration/change-password.html', success_url='done'),  name='change-password'),
-    path('accounts/change-password/done', auth_views.PasswordChangeDoneView.as_view(template_name='registration/change-password-done.html'), name="change-password-done"),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("dashboard/", views.dashboard, name="dashboard"),
     path("register/", views.register, name="register")
 ]
