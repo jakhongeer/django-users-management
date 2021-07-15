204e23edccd765973548ac198d03a4598b1ab520
Jahongir Nagmatov <jahongirnagmatov@gmail.com>
2021-07-15 18:30:24 +0500

    model fixes

--- shopify/models.py
+++ shopify/models.py
@@ -10,6 +10,7 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, null=True, blank=True)
@@ -22,14 +23,22 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.id
+        return self.name
+
 
 class Orderedtem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
+
 class Order(models.Model):
+    id = models.AutoField(primary_key=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
     start_date = models.DateTimeField(auto_now_add=True)
     ordered_date = models.DateTimeField()
     ordered = models.BooleanField(default=False)
+
+    def __int__(self):
+        return self.id
+
+    
