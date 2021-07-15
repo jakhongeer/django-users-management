61defc9c99245bc0bc39f1c4b854ea7f7940e3d9
Jahongir Nagmatov <jahongirnagmatov@gmail.com>
2021-07-15 14:04:36 +0500

    create category and product models

--- shopify/models.py
+++ shopify/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, null=True, blank=True)
+    brand = models.CharField(max_length=255, null=True, blank=True)
+    description = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+    price = models.DecimalField(null=True, blank=True)
+    quantity = models.PositiveIntegerField(null=True, blank=True, default=0)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=True)
+
+    def __str__(self):
+        return self.id
+
+
+
+
