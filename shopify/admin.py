5b8e7083b0323786b11232035a17c4baba9e7ce8
Jahongir Nagmatov <jahongirnagmatov@gmail.com>
2021-07-15 15:28:28 +0500

    admin registers models

--- shopify/admin.py
+++ shopify/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
-
+from .models import Category, Product, Order, Orderedtem
 # Register your models here.
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Orderedtem)
+class OrderedtemAdmin(admin.ModelAdmin):
+    pass
