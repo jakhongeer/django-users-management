4bb8da9209fe5312d04fdee741d8c109813a622c
Jahongir Nagmatov <jahongirnagmatov@gmail.com>
2021-07-15 15:13:33 +0500

    create order and orderItem models

--- shopify/models.py
+++ shopify/models.py
@@ -1,12 +1,15 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
+
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, null=True, blank=True)
@@ -21,6 +24,12 @@ class Product(models.Model):
     def __str__(self):
         return self.id
 
+class Orderedtem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
-
-
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
