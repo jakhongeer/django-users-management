9e2501849f6771c830d014c9552e8f5d47edf6e5
Jakhongir <jahongirnagmatov@gmail.com>
2021-07-17 06:47:27 -0700

    configure templates and its urls

--- shopify/urls.py
+++ shopify/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = "shopify"
 
 urlpatterns = [
-    path('products/', views.getProducts, name="products")
+    path('products/', views.getProducts, name="products"),
+    path('store/', views.store, name="store"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
 ]
--- shopify/views.py
+++ shopify/views.py
@@ -8,3 +8,15 @@ def getProducts(request):
     }
 
     return render(request, "product_list.html", context)
+
+def store(request):
+    context = {}
+    return render(request, 'store/store.html', context)
+
+def cart(request):
+    context = {}
+    return render(request, 'store/cart.html', context)
+
+def checkout(request):
+    context = {}
+    return render(request, 'store/checkout.html', context)
