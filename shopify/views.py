c7321cce7a87701afec2de664a481a28dc7e2a97
Jakhongir <jahongirnagmatov@gmail.com>
2021-07-23 18:11:04 +0500

    make cart template dynamic with datas

--- shopify/views.py
+++ shopify/views.py
@@ -15,7 +15,13 @@ def store(request):
     return render(request, 'store/store.html', context)
 
 def cart(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, ordered=False) #see the documentation https://docs.djangoproject.com/en/3.2/ref/models/querysets/#get-or-create
+        products = order.ordereditem_set.all()
+    else:
+        products = []
+    context = {'products': products}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
