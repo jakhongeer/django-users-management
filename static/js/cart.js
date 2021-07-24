3962133ae43e12b7ac71b2179e1e0aaa781a99b3
Jakhongir <jahongirnagmatov@gmail.com>
2021-07-24 17:04:05 +0500

    retrieve the user and product id when add to cart clicks

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,1 +1,16 @@
-console.log('Hello js')
+var updateBtns = document.getElementsByClassName('add-to-cart')
+
+for (i = 0; i < updateBtns.length; i++) {
+    updateBtns[i].addEventListener('click', function(){
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log('productId:', productId, 'Action:', action)
+
+        console.log('USER:', user)
+        if (user == 'AnonymousUser'){
+            console.log('User is not authenticated')
+        }else {
+            console.log('User is authenticated, sending data...')
+        }
+    })
+}
