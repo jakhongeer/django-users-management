ae45f80cd993168c4837a1ab14010f35fceac3bf
Jakhongir <jahongirnagmatov@gmail.com>
2021-07-24 16:38:16 +0500

    make the template dynamic

--- shopify/views.py
+++ shopify/views.py
@@ -26,5 +26,12 @@ def cart(request):
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, ordered=False) #see the documentation https://docs.djangoproject.com/en/3.2/ref/models/querysets/#get-or-create
+        products = order.orderedproduct_set.all()
+    else:
+        products = []
+        order = {'get_cart_total':0, 'get_cart_products':0}
+    context = {'products': products, 'order': order}
     return render(request, 'store/checkout.html', context)
